3c81e4add333977b8e79fdf19be0276666179ee5
Abdulhamid PAKYOL <pakyol.tc@gmail.com>
2022-12-18 15:25:36 +0300

    created instance variables,constractor, toString.

--- src/day38_Inheritance/CarTask/Car.java
+++ src/day38_Inheritance/CarTask/Car.java
@@ -1,5 +1,37 @@
 package day38_Inheritance.CarTask;
 
 public class Car {
+public String brand,model,color;
+public int year, mile;
+public double price;
 
+    public Car(String brand, String model, String color, int year, int mile, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.year = year;
+        this.mile = mile;
+        this.price = price;
+    }
+    public void drive(){
+
+    }
+    public void start(){
+
+    }
+    /*public void fly(){
+
+    } as flying is not the common facility for all types of car currently.
+     */
+
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", year=" + year +
+                ", mile=" + mile +
+                ", price=" + price +
+                '}';
+    }
 }
